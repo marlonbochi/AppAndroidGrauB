fe85b403348724a4a938218f81b8924fa68c6a9c
Marlon Bochi <marlon.bueno@inevent.us>
2019-11-26 16:32:39 -0300

    subindo o app

---
+++ app/src/main/java/br/com/marlonbochi/graub/LoginActivity.java
@@ -0,0 +1,110 @@
+package br.com.marlonbochi.graub;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.gson.Gson;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText editEmail, editPassword;
+    Button btnSignIn, btnCreate;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editEmail = findViewById(R.id.editEmail);
+        editPassword = findViewById(R.id.editPassword);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnCreate = findViewById(R.id.btnCreate);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser.getEmail() != "") {
+            goToHome();
+        }
+
+        btnCreate.setVisibility(View.INVISIBLE);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = editEmail.getText().toString();
+                String password = editPassword.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("SuccessAuth", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            goToHome();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("ErrorAuth", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            btnCreate.setVisibility(View.VISIBLE);
+                        }
+
+                        // ...
+                    }
+                });
+            }
+        });
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = editEmail.getText().toString();
+                String password = editPassword.getText().toString();
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d("SuccessAuth", "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                btnCreate.setVisibility(View.INVISIBLE);
+                                goToHome();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("ErrorAuth", "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(LoginActivity.this, "Authentication failed." + task.getException().getMessage(),
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+            }
+        });
+    }
+
+    protected void goToHome() {
+
+        Intent sendData = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(sendData);
+    }
+}
---
+++ app/src/main/java/br/com/marlonbochi/graub/MainActivity.java
@@ -0,0 +1,182 @@
+package br.com.marlonbochi.graub;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnGetAgenda, btnSaveFirebase;
+    private Cursor cursor;
+    ListView listViewAgenda, listViewFirebase;
+    FirebaseUser currentUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnGetAgenda = findViewById(R.id.btnGetAgenda);
+        btnSaveFirebase = findViewById(R.id.btnSaveFirebase);
+        listViewAgenda = findViewById(R.id.listViewAgenda);
+        listViewFirebase = findViewById(R.id.listViewFirebase);
+
+        Intent receiveData = getIntent();
+        Gson gson = new Gson();
+
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (currentUser.getEmail() == null) {
+            goToLogin();
+        }
+
+        ActivityCompat.requestPermissions(MainActivity.this,new String[]
+                {Manifest.permission.READ_CONTACTS},1);
+
+
+        btnGetAgenda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri caminho = ContactsContract.Contacts.CONTENT_URI;
+                ContentResolver contentResolver = MainActivity.this.getContentResolver();
+                cursor = contentResolver.query(caminho, null, null, null,ContactsContract.Contacts.DISPLAY_NAME);
+
+                if (cursor.getCount() > 0)
+                {
+                    List<String> contacts = new ArrayList<>();
+                    while (cursor.moveToNext())
+                    {
+                        String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                        String has_phone = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+                        String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                        if (!has_phone.endsWith("0")) {
+
+                            String cellphone = "";
+                            Cursor cp = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
+
+                            if (cp != null && cp.moveToFirst()) {
+                                cellphone = cp.getString(cp.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                                cp.close();
+                            }
+
+                            name += " - " + cellphone;
+                        }
+                        contacts.add(name);
+                    }
+
+                    listViewAgenda.setAdapter(new ArrayAdapter<String>(MainActivity.this,
+                            android.R.layout.simple_list_item_1, contacts));
+                }
+            }
+        });
+
+
+        btnSaveFirebase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                Map<String, Object> contact = new HashMap<>();
+
+                for (int i = 0; i < listViewAgenda.getChildCount(); i++) {
+                    // Get row's spinner
+                    View listItem = listViewAgenda.getChildAt(i);
+
+                    TextView item = (TextView)listItem;
+
+                    contact.put("idUser", currentUser.getUid());
+                    contact.put("contact", item.getText());
+
+                    db.collection("agenda")
+                            .add(contact)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Log.d("Added into firebase", "DocumentSnapshot added with ID: " + documentReference.getId());
+
+                                    getInfoAgendaFirebase();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w("ErrorAdd", "Error adding document", e);
+                                }
+                            });
+                }
+            }
+        });
+    }
+
+    protected void goToLogin() {
+
+        FirebaseAuth.getInstance().signOut();
+
+        Intent sendData = new Intent(MainActivity.this, LoginActivity.class);
+
+        startActivity(sendData);
+    }
+
+    protected void getInfoAgendaFirebase() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("agenda")
+            .get()
+            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                    List<String> contacts = new ArrayList<>();
+
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Map<String, Object> data = document.getData();
+                            contacts.add(data.get("contact").toString());
+
+                        }
+
+                        listViewFirebase.setAdapter(new ArrayAdapter<String>(MainActivity.this,
+                                android.R.layout.simple_list_item_1, contacts));
+                    } else {
+                        Log.w("errorGetAgenda", "Error getting documents.", task.getException());
+                    }
+                }
+            });
+    }
+
+}
